3009de0ebb1a92d22257f1817fb905793a6f6e3d
Dev-Tams <tammyalemu@gmail.com>
2025-05-22 19:53:36 +0100

    tested the func

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dev-tams/go-auth/auth"
+)
+
+func main() {
+	// Step 1: Hash a password
+	hashed, err := auth.HashPassword("my-secret")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Hashed password:", hashed)
+
+	// Step 2: Verify login
+	err = auth.CheckPassword(hashed, "my-secret")
+	if err != nil {
+		fmt.Println("❌ Incorrect password")
+	} else {
+		fmt.Println("✅ Password is correct")
+	}
+}

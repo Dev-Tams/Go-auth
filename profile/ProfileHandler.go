08587c6b19ab3362f3550f99f6b8ace0db0df0a5
Dev-Tams <tammyalemu@gmail.com>
2025-05-30 23:58:14 +0100

    added the profile folder

---
+++ profile/ProfileHandler.go
@@ -0,0 +1,27 @@
+package profile
+
+import(
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"go-auth/auth"
+)
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request){
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	userID, err := auth.ValidateJWT(token)
+	if err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"user_id": userID})
+}
---
+++ profile/RegisterUser.go
@@ -0,0 +1,62 @@
+package profile
+
+import(
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"go-auth/auth"
+
+	
+)
+
+func RegisterUser(w http.ResponseWriter, r *http.Request){
+	//check method
+	if r.Method != http.MethodPost{
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		http.Error(w, "Invalid Body param", http.StatusBadRequest)
+		return
+	}
+
+	user, err := auth.RegisterUser(req.Email, req.Password)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func LoginUser(w http.ResponseWriter, r *http.Request){
+		//check method
+	if r.Method != http.MethodPost{
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil{
+		http.Error(w, "Invalid Body param", http.StatusBadRequest)
+		return
+	}
+
+	token, err := auth.LoginUser(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, "Login failed", http.StatusUnauthorized)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
